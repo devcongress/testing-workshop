5017371ed044080e7c9b007b12b7b54ff646bed4
Andrew Smith <a.smith@silentworks.co.uk>
2017-12-15 16:47:34 +0000

    Add in spec file

---
+++ spec/cart.spec.js
@@ -0,0 +1,52 @@
+describe('Cart', () => {
+    it('should be initiable', () => {
+      
+    })
+  
+    it('should add item to cart', () => {
+
+    })
+  
+    it('should update item to cart', () => {
+      
+    })
+  
+    it('should remove item from cart', () => {
+      
+    })
+  
+    it('should have a list of all item in cart', () => {
+      
+    })
+  
+    it('should give the total value of items in the cart', () => {
+
+    })
+  
+    it('should remove all items from the cart', () => {
+      
+    })
+  })
+  
+  describe('Item', () => {
+    it('should be initiable', () => {
+      
+    });
+  
+    it('should update item', () => {
+      
+    })
+  
+    it('should get item total', () => {
+      
+    })
+  
+    it('should get item total with more than 1 as the quantity', () => {
+      
+    })
+  
+    it('should contain name, quantity and price', () => {
+      
+    })
+  })
+  
